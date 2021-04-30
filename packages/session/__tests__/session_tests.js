ee02b072edcd938300dc102c093b63babbfb6246
Harry Adel <harryadelb@gmail.com>
2021-04-30 17:04:27 +0200

    Add mongo package to session

--- packages/session/__tests__/session_tests.js
+++ packages/session/__tests__/session_tests.js
@@ -1,58 +1,6 @@
 import { Tracker } from 'standalone-tracker';
+import { MongoID } from 'standalone-mongo-id';
 import { Session } from '../src/session';
-
-const MongoID = {};
-
-MongoID._looksLikeObjectID = (str) => str.length === 24 && str.match(/^[0-9a-f]*$/);
-
-MongoID.ObjectID = class ObjectID {
-  constructor(hexString) {
-    // random-based impl of Mongo ObjectID
-    if (hexString) {
-      hexString = hexString.toLowerCase();
-      if (!MongoID._looksLikeObjectID(hexString)) {
-        throw new Error('Invalid hexadecimal string for creating an ObjectID');
-      }
-      // meant to work with _.isEqual(), which relies on structural equality
-      this._str = hexString;
-    } else {
-      this._str = Random.hexString(24);
-    }
-  }
-
-  equals(other) {
-    return other instanceof MongoID.ObjectID
-    && this.valueOf() === other.valueOf();
-  }
-
-  toString() {
-    return `ObjectID("${this._str}")`;
-  }
-
-  clone() {
-    return new MongoID.ObjectID(this._str);
-  }
-
-  typeName() {
-    return 'oid';
-  }
-
-  getTimestamp() {
-    return Number.parseInt(this._str.substr(0, 8), 16);
-  }
-
-  valueOf() {
-    return this._str;
-  }
-
-  toJSONValue() {
-    return this.valueOf();
-  }
-
-  toHexString() {
-    return this.valueOf();
-  }
-};
 
 test('session - setDefault', () => {
   Session.setDefault('def', 'argyle');
