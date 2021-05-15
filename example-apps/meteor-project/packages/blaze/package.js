2b3e2c891fb65c548e974abfa349de2d603ec8d7
Harry Adel <harryadelb@gmail.com>
2021-05-15 21:34:26 +0200

    Fix package name

--- example-apps/meteor-project/packages/blaze/package.js
+++ example-apps/meteor-project/packages/blaze/package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: "blaze",
-  version: "2.3.0",
+  name: 'znewsham:blaze',
+  version: '2.3.0',
   // Brief, one-line summary of the package.
-  summary: "",
+  summary: '',
   // URL to the Git repository containing the source code for this package.
-  git: "",
+  git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: "README.md"
+  documentation: 'README.md',
 });
 
 // NOTE: intentionally NOT npm depending this - if we do, meteor will bundle meteor-blaze-runtime in here, we don't want it to - we want meteor-blaze-runtime to be bundled with the rest of the npm modules.
@@ -18,32 +18,32 @@ Npm.depends({
 */
 
 Package.onUse((api) => {
-  api.use("ecmascript");
-  api.use("reactive-var");
-  api.use("isobuild:compiler-plugin@1.0.0");
-  api.use("jquery", "client");
-  api.mainModule("blaze-client.js", "client", { lazy: true });
-  api.mainModule("blaze-server.js", "server", { lazy: true });
+  api.use('ecmascript');
+  api.use('reactive-var');
+  api.use('isobuild:compiler-plugin@1.0.0');
+  api.use('jquery', 'client');
+  api.mainModule('blaze-client.js', 'client', { lazy: true });
+  api.mainModule('blaze-server.js', 'server', { lazy: true });
   // api.mainModule('blaze-client.js', 'server', { lazy: true });
 });
 
 Package.onTest((api) => {
-  api.use("ecmascript");
-  api.use("tinytest");
-  api.use("znewsham:blaze");
-  api.mainModule("blaze-tests.js");
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('znewsham:blaze');
+  api.mainModule('blaze-tests.js');
 });
 
 Package.registerBuildPlugin({
-  name: "compileTemplatesBatch",
+  name: 'compileTemplatesBatch',
   use: [
-    "ecmascript",
-    "caching-html-compiler@1.1.2"
+    'ecmascript',
+    'caching-html-compiler@1.1.2',
   ],
   sources: [
-    "blaze.js"
+    'blaze.js',
   ],
   npmDependencies: {
-    "meteor-blaze-compiler": "1.0.12"
-  }
+    'meteor-blaze-compiler': '1.0.12',
+  },
 });
