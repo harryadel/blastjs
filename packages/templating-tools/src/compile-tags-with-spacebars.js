ff1cef77c176de1492d1f37a7f5752285f6e4a86
harryadel <harryadelb@gmail.com>
2021-10-19 15:18:11 +0200

    Update templating-tools package-lock

--- packages/templating-tools/src/compile-tags-with-spacebars.js
+++ packages/templating-tools/src/compile-tags-with-spacebars.js
@@ -1,10 +1,10 @@
 import isEmpty from 'lodash.isempty';
-import { SpacebarsCompiler } from 'spacebars-compiler';
+import { SpacebarsCompiler } from '@blastjs/spacebars-compiler';
 import { generateBodyJS, generateTemplateJS } from './code-generation';
 import { throwCompileError } from './throw-compile-error';
 
 export function compileTagsWithSpacebars(tags, hmrAvailable) {
-  var handler = new SpacebarsTagCompiler();
+  const handler = new SpacebarsTagCompiler();
 
   tags.forEach((tag) => {
     handler.addTagToResults(tag, hmrAvailable);
@@ -13,14 +13,13 @@ export function compileTagsWithSpacebars(tags, hmrAvailable) {
   return handler.getResults();
 }
 
-
 class SpacebarsTagCompiler {
   constructor() {
     this.results = {
       head: '',
       body: '',
       js: '',
-      bodyAttrs: {}
+      bodyAttrs: {},
     };
   }
 
@@ -34,23 +33,22 @@ class SpacebarsTagCompiler {
     // do we have 1 or more attributes?
     const hasAttribs = !isEmpty(this.tag.attribs);
 
-    if (this.tag.tagName === "head") {
+    if (this.tag.tagName === 'head') {
       if (hasAttribs) {
-        this.throwCompileError("Attributes on <head> not supported");
+        this.throwCompileError('Attributes on <head> not supported');
       }
 
       this.results.head += this.tag.contents;
       return;
     }
 
-
     // <body> or <template>
 
     try {
-      if (this.tag.tagName === "template") {
-        const name = this.tag.attribs.name;
+      if (this.tag.tagName === 'template') {
+        const { name } = this.tag.attribs;
 
-        if (! name) {
+        if (!name) {
           this.throwCompileError("Template has no 'name' attribute");
         }
 
@@ -63,25 +61,26 @@ class SpacebarsTagCompiler {
         const renderFuncCode = SpacebarsCompiler.compile(this.tag.contents, {
           whitespace,
           isTemplate: true,
-          sourceName: `Template "${name}"`
+          sourceName: `Template "${name}"`,
         });
 
         this.results.js += generateTemplateJS(
-          name, renderFuncCode, hmrAvailable);
-      } else if (this.tag.tagName === "body") {
+          name, renderFuncCode, hmrAvailable,
+        );
+      } else if (this.tag.tagName === 'body') {
         const { whitespace = '', ...attribs } = this.tag.attribs;
         this.addBodyAttrs(attribs);
 
         const renderFuncCode = SpacebarsCompiler.compile(this.tag.contents, {
           whitespace,
           isBody: true,
-          sourceName: "<body>"
+          sourceName: '<body>',
         });
 
         // We may be one of many `<body>` tags.
         this.results.js += generateBodyJS(renderFuncCode, hmrAvailable);
       } else {
-        this.throwCompileError("Expected <template>, <head>, or <body> tag in template file", tagStartIndex);
+        this.throwCompileError('Expected <template>, <head>, or <body> tag in template file', tagStartIndex);
       }
     } catch (e) {
       if (e.scanner) {
@@ -102,7 +101,8 @@ class SpacebarsTagCompiler {
       // attributes on results.bodyAttrs
       if (this.results.bodyAttrs.hasOwnProperty(attr) && this.results.bodyAttrs[attr] !== val) {
         this.throwCompileError(
-          `<body> declarations have conflicting values for the '${attr}' attribute.`);
+          `<body> declarations have conflicting values for the '${attr}' attribute.`,
+        );
       }
 
       this.results.bodyAttrs[attr] = val;
