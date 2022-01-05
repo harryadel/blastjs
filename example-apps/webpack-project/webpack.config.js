c8d34de15d8e6a4a84b534adba6af7984ca51447
harryadel <harryadelb@gmail.com>
2022-01-05 02:53:27 +0200

    Add path-browserify

--- example-apps/webpack-project/webpack.config.js
+++ example-apps/webpack-project/webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
       https: require.resolve('https-browserify'),
       os: require.resolve('os-browserify/browser'),
       stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
     },
   },
   module: {
