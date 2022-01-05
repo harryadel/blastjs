208404cd551980122dd53a1b154b8eb3a9bd9e66
harryadel <harryadelb@gmail.com>
2022-01-05 02:24:10 +0200

    Install necessary packages and config webpack

--- example-apps/webpack-project/webpack.config.js
+++ example-apps/webpack-project/webpack.config.js
@@ -1,18 +1,45 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      process: 'process/browser',
+    },
+    fallback: {
+      fs: false,
+      url: false,
+      buffer: false,
+      child_process: false,
+      net: false,
+      tls: false,
+      zlib: false,
+      assert: require.resolve('assert-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      stream: require.resolve('stream-browserify'),
+    },
+  },
   module: {
     rules: [
-     {
-       test: /\.html$/,
-       use: "blaze-loader"
-     }
-    ]
+      {
+        test: /\.html$/,
+        use: '@blastjs/blast-loader',
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
 };
