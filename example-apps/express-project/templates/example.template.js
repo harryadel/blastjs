8858dae4989e4bc4fc0bfbc6ec103bf7a85f77f9
harryadel <harryadelb@gmail.com>
2022-01-11 00:13:35 +0200

    Lint example.template.js

--- example-apps/express-project/templates/example.template.js
+++ example-apps/express-project/templates/example.template.js
@@ -4,14 +4,20 @@ const { Template } = require('@blastjs/templating-runtime');
 
 Template.__checkName('example');
 
-Template.example = new Template('Template.example',
+Template.example = new Template(
+  'Template.example',
   (function () {
     const view = this;
     return ['This is an example\n  ',
-      Blast.If(() => Spacebars.call(view.lookup('name')),
+      Blast.If(
+        () => Spacebars.call(view.lookup('name')),
         (() => ['\n    Hello ',
-          Blast.View('lookup:name',
-            () => Spacebars.mustache(view.lookup('name'))),
+          Blast.View(
+            'lookup:name',
+            () => Spacebars.mustache(view.lookup('name')),
+          ),
           '\n  ']),
-        (() => '\n    Stick ?name=yourName in the URL\n  '))];
-  }));
+        (() => '\n    Stick ?name=yourName in the URL\n  '),
+      )];
+  }),
+);
