538a7a0cc5e7ee92806a0f9d78a3d9f03aa62566
harryadel <harryadelb@gmail.com>
2022-01-11 00:22:08 +0200

    Edit package.js

---
+++ example-apps/meteor-project/packages/blast/README.md
@@ -0,0 +1,3 @@
+# harry97:blast
+
+This is a very simple wrapper package that sets the global variables needed for Blast to work.
--- example-apps/meteor-project/packages/blast/package.js
+++ example-apps/meteor-project/packages/blast/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/harryadel/blastjs/tree/main/example-apps/meteor-project/packages/blast',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
