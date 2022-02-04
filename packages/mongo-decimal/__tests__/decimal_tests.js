bff4e768b02337d7801267208b6d73277cbc25e1
harryadel <harryadelb@gmail.com>
2022-02-04 21:58:37 +0200

    Add mongo-decimal

---
+++ packages/mongo-decimal/__tests__/decimal_tests.js
@@ -0,0 +1,9 @@
+// test('mongo-decimal - insert/find Decimal', (test) => {
+//   const coll = new Mongo.Collection('mongo-decimal');
+//   const pi = Decimal('3.141592653589793');
+
+//   coll.insert({ pi });
+//   const found = coll.findOne({ pi });
+
+//   test.equal(found.pi, pi);
+// });
